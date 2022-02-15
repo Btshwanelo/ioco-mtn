a2a17b3068d6a829f80242add72cb63ca367726a
Btshwanelo <bucibot@gmail.com>
2022-02-15 16:42:27 +0200

    added appvbar
    installed material ui and added app bar component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import Appbar from "./components/Appbar";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Appbar />
+        </Grid>
+        <Grid item xs={12}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
